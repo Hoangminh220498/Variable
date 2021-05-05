00e6e82622049d5ff64209cb69ff0fceb7f42d8d
= <=>
2021-05-05 21:29:17 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,24 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + " " + lastName );
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		
+		//Khai bao hang. hang phai viet hoa
+		final float PI = 3.14f;
+
+
+	}
+
+}
